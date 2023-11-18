ef03d2e691e5bb12ce7973c4159b40deb87c9ac0
Berendea Andrei <berendeaandrei03@gmail.com>
2023-11-18 22:40:43 +0200

    added map

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {GoogleMapsModule} from "@angular/google-maps";
 
 @Component({
@@ -10,5 +10,16 @@ import {GoogleMapsModule} from "@angular/google-maps";
   styleUrl: './home.component.scss'
 })
 export default class HomeComponent {
+  markers: google.maps.LatLngLiteral[] = [];
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
 
+  addLocation($event: google.maps.MapMouseEvent | google.maps.IconMouseEvent) {
+    const latLngJson = $event.latLng?.toJSON();
+    console.log(latLngJson)
+    if (!latLngJson) {
+      console.warn("lat lng undefined!")
+      return;
+    }
+    this.markers.push(latLngJson)
+  }
 }
